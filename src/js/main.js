10ebe7c8c276b9a022acdc994b53e9b7e7fdff3b
o5k1 <tombolato.andrea@gmail.com>
2017-03-21 15:25:36 +0100

    Code adapted to meet requirements 2: could be inserted more than a clicker and each one must have its own name and counter. Avoid repetition as much as possible

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,45 @@
-$( document ).ready(function(){
-	var incrementButton = $('#increment-button');
-	var clickCounter = $('#click-counter');
+var Clicker = function(parentEl, name){
+	this.el = null;
+	this.parentEl = parentEl;
+	this.name = name;
+	this.template = $('#clickerTemplate').html();
+	this.ui = {
+		"name" : null,
+		"counter" : null,
+		"button" : null
+	};
+	
+	this.render = function () {
+		this.el = $(this.template);
+		parentEl.append(this.el);
+		this.el.attr('id', 'clicker-' + this.name);
+		this.ui.name = this.el.find('.clicker__name');
+		this.ui.counter = this.el.find('.clicker__counter');
+		console.log(this.ui.counter);
+		this.ui.button = this.el.find('.clicker__button');
+		this.setName(this.name);
 
-	var onIncrementButtonClicked = function(){
-		var count = clickCounter.html();
-		clickCounter.html(++count);
+		// Events handlers
+		this.ui.button.click({that : this}, this.onIncrementButtonClicked);
+	}
+
+	this.onIncrementButtonClicked = function(options){
+		var that = options.data.that;
+		var count = that.ui.counter.html();
+		that.ui.counter.html(++count);
 	};
 
-	incrementButton.click(onIncrementButtonClicked);
+	this.setName = function (name) {
+		this.ui.name.text(name);
+	}
+}
+
+$( document ).ready(function(){
+	var root = $('#content');
+
+	var clickerFirst = new Clicker(root, 'First');
+	clickerFirst.render();
+	var clickerSecond = new Clicker(root, 'Second');
+	clickerSecond.render();
 });
 
